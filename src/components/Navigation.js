0b75800be24d0188a08fbcb3c085bacaaf08c559
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-12-08 16:29:35 +0100

    add SocialNetwork comp and anim

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,26 @@ const Navigation = () => {
   return (
     <div className="navigation">
       <ul>
-        <NavLink to="/" exact className="hover" activeClassName="nav-active" />
+        <NavLink to="/" exact className="hover" activeClassName="nav-active">
+          <li>Accueil</li>
+        </NavLink>
+
+        <li className="nav-portfolio">Portfolio
+          <ul className="nav-projects">
+            <NavLink to="/project-1" activeClassName="nav-active" className="hover">
+              <li>projet 1</li>
+            </NavLink><NavLink to="/project-2" activeClassName="nav-active" className="hover">
+              <li>projet 2</li>
+            </NavLink><NavLink to="/project-3" activeClassName="nav-active" className="hover">
+              <li>projet 3</li>
+            </NavLink><NavLink to="/project-4" activeClassName="nav-active" className="hover">
+              <li>projet 4</li>
+            </NavLink>
+          </ul>
+        </li>
+        <NavLink to="/contact" exact className="hover" activeClassName="nav-active">
+          <li>Contact</li>
+        </NavLink>
       </ul>
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navigation from '../components/Navigation'
+import SocialNetwork from '../components/SocialNetwork'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Navigation />
+      <SocialNetwork />
     </div>
   )
 }
