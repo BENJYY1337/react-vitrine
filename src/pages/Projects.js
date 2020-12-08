eeb5dc702064db9ac7c315b1246bfc9491360f35
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-12-08 17:02:28 +0100

    add scroll navigation for the prev and next page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import ButtonsBottom from '../components/ButtonsBottom'
+import Navigation from '../components/Navigation'
 
 export const Project1 = () => {
   return (
     <main>
       <div className="project">
+        <Navigation />
         Project 1
+        <ButtonsBottom left={'/'} right={'/project-2'} />
       </div>
     </main>
   )
@@ -14,7 +18,9 @@ export const Project2 = () => {
   return (
     <main>
       <div className="project">
+        <Navigation />
         Project 2
+        <ButtonsBottom left={'/project-1'} right={'/project-3'} />
       </div>
     </main>
   )
@@ -24,7 +30,9 @@ export const Project3 = () => {
   return (
     <main>
       <div className="project">
+        <Navigation />
         Project 3
+        <ButtonsBottom left={'/project-2'} right={'/project-4'} />
       </div>
     </main>
   )
@@ -34,7 +42,9 @@ export const Project4 = () => {
   return (
     <main>
       <div className="project">
+        <Navigation />
         Project 4
+        <ButtonsBottom left={'/project-3'} right={'/contact'} />
       </div>
     </main>
   )
